2673f2988be4c545098042a42bfd0894c65bdd11
Jason Frame <jason@onehackoranother.com>
2015-10-18 23:48:06 +0100

    tabs -> spaces

--- handlers.js
+++ handlers.js
@@ -1,47 +1,47 @@
 module.exports = function(ctx) {
 
-	ctx.motion([
-		'north',
-		'northeast',
-		'east',
-		'southeast',
-		'south',
-		'southwest',
-		'west',
-		'northwest',
-		'up',
-		'down'
-	]);
+    ctx.motion([
+        'north',
+        'northeast',
+        'east',
+        'southeast',
+        'south',
+        'southwest',
+        'west',
+        'northwest',
+        'up',
+        'down'
+    ]);
 
-	ctx.aliases({
-		north: ['n'],
-		northeast: ['ne'],
-		east: ['e'],
-		southeast: ['se'],
-		south: ['s'],
-		southwest: ['sw'],
-		west: ['w'],
-		northwest: ['nw'],
-		up: ['u'],
-		down: ['down']
-	});
+    ctx.aliases({
+        north: ['n'],
+        northeast: ['ne'],
+        east: ['e'],
+        southeast: ['se'],
+        south: ['s'],
+        southwest: ['sw'],
+        west: ['w'],
+        northwest: ['nw'],
+        up: ['u'],
+        down: ['down']
+    });
 
-	// ctx.rewrite('go <direction:motion>', '$direction');
+    // ctx.rewrite('go <direction:motion>', '$direction');
 
-	ctx.handler(':motion', {
-		handler: function(command, actor, game) {
-			console.log(arguments);
-		}
-	});
+    ctx.handler(':motion', {
+        handler: function(command, actor, game) {
+            console.log(arguments);
+        }
+    });
 
-	ctx.handler('get *', {
-		guard: function() {
+    ctx.handler('get *', {
+        guard: function() {
 
-		},
-		handler: function(command, actor, game) {
+        },
+        handler: function(command, actor, game) {
 
-		}
-	});
+        }
+    });
 
 
 }
